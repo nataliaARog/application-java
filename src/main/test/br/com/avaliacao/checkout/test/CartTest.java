d9aef0c320a9618bbadbf43925ee995a92cb95f2
root <root@natalia-Inspiron-5537>
2016-08-22 19:12:55 -0300

    avaliação

---
+++ src/main/test/br/com/avaliacao/checkout/test/CartTest.java
@@ -0,0 +1,18 @@
+package br.com.avaliacao.checkout.test;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import br.com.avaliacao.AvaliacaoJavaApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(AvaliacaoJavaApplication.class)
+public class CartTest {
+	
+	
+
+}
